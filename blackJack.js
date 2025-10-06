58aa9aca267bbd391a8d68177661441993952b0a
mon <mohansp119@gmail.com>
2025-10-06 14:58:50 +0530

    Updated working in Blackjack game

---
+++ blackJack.js
@@ -0,0 +1,41 @@
+// Black Jack game
+
+let firstCard = 1,
+  secondCard = 11;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("mes-el");
+let sumEl = document.getElementById("sum");
+let cardEl = document.querySelector("#cards");
+
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? ";
+  } else if (sum === 21) {
+    message = "hooray! Black Jack";
+    hasBlackJack = true;
+  } else {
+    message = "Out of Game";
+    isAlive = false;
+  }
+  messageEl.innerHTML = message;
+}
+console.log(isAlive);
+
+// Cash Out
+console.log(hasBlackJack);
+
+function newCard() {
+  let card = 8;
+  sum += card;
+  startGame();
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+let firstCard = 2,
+//   secondCard = 11;
+
+// let sum = firstCard + secondCard;
+
+// if (sum < 21) {
+//   console.log("Do you want to draw a new card? ");
+// } else if (sum === 21) {
+//   console.log("hooray! Black Jack");
+// } else {
+//   console.log("Out of Game");
+// }
