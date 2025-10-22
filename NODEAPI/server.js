59e6ff65e38df70d361ac1d6eec6700cc22b8992
mon <mohansp119@gmail.com>
2025-10-22 13:10:57 +0530

    Changes made in diectory

--- NODEAPI/server.js
+++ NODEAPI/server.js
@@ -1,23 +1,21 @@
 import http from "node:http";
-import { getData } from "./db.js";
-import { response } from "./frontend.js";
+import getData from './db.js'
 
-const PORT = 8000;
+const PORT = 8000
 
-const server = http.createServer(async (req, res) => {
-  const destinations = await getData();
+const server = http.createServer( async (req, res) => {
 
-  if (req.url === "/api" && req.method === "GET") {
-    response.setHeader("Content-Type", "application/json");
-    response.statusCode = 200;
-    res.end(JSON.stringify(destinations));
-  } else if(req.url === `/api/continent/${}`) {
+    const destinations = await getData()
 
-  } else {
-    response.setHeader("Content-Type", "application/json");
-    response.statusCode = 404;
-    res.end(`Cannot ${req.method} ${req.url}`);
-  }
+    if (req.url === '/api' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200
+        res.end(JSON.stringify(destinations))
+    } else if(req.url.startsWith('/api/continent')) {
+        
+    }
+    res.end("Hello from the server!");
 });
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+server.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
